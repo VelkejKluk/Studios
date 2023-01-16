e623211b257cf4864bf1806f374354d001d63e06
Josef Navrátil <josef.navratil@educhem.cz>
2023-01-16 09:08:33 +0100

    Přidejte soubory projektu.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] poleStudii = { "Bohemia Interactive", "Warhorse Studios", "SCS Software", "2K Czech", "Madfinger Games" };
+            Studia.ItemsSource = poleStudii; 
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Image.Source = new BitmapImage(new Uri($@"Images/{Studia.SelectedItem}.png", UriKind.Relative));
+        }
+    }
+}
